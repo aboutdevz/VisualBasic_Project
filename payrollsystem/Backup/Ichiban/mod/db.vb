ad1a202271552e2159362d680cca336b2f370293
aboutdevz <aboutdevz@gmail.com>
2020-04-13 03:21:45 +0800

    remove hhuhu.txt

--- payrollsystem/Backup/Ichiban/mod/db.vb
+++
@@ -1,6 +0,0 @@
-﻿Module db
-    Public Function myconn() As OleDb.OleDbConnection
-        Return New OleDb.OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "\enterprisedb.mdb")
-    End Function
-
-End Module
