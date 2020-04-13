7e8d781a428da67d43961ed2ece67d81a4eeeac9
aboutdevz <aboutdevz@gmail.com>
2020-04-14 00:53:11 +0800

    Added Stopwatch Project

---
+++ Counting/Counting/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        stopwatch.Start()
+    End Sub
+
+    Private stopwatch As New Stopwatch
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim elapsed As TimeSpan = stopwatch.Elapsed
+        Label1.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", Math.Floor(elapsed.Hours), elapsed.Minutes, elapsed.Seconds, elapsed.Milliseconds)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Stop()
+        stopwatch.Stop()
+    End Sub
+End Class
